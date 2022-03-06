1ec77649fa83a729d254fe6ed90de237a8963d32
pj8912 <jpintofun2b@gmail.com>
2022-03-06 07:38:56 +0530

    intial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>css_grids</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="item">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto similique nisi doloribus nihil vel, sapiente quod blanditiis aliquid perferendis voluptatibus? Soluta fuga qui explicabo reiciendis, repellendus saepe nihil necessitatibus iure.
+        </div>
+        <div class="item">I2</div>
+        <div class="item">I3</div>
+        <div class="item">I4</div>
+        <div class="item">I5</div>
+        <div class="item">I6</div>
+        <div class="item">I7</div>
+        <div class="item">I8</div>
+        <div class="item">I9</div>
+
+    </div>
+</body>
+
+</html>
